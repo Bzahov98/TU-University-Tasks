581fcce3d845b882b228e40174667b3ece39269a
Bzahov98 <bzahov1998@gmail.com>
2018-11-22 16:54:59 +0200

    [FINNISHEDss]

--- Java-PIK III/RaceCompetition/src/com/tu/bzahov/App.java
+++ Java-PIK III/RaceCompetition/src/com/tu/bzahov/App.java
@@ -1,5 +1,8 @@
 package com.tu.bzahov;
 
+import com.tu.bzahov.competitions.FootballCompetition;
+import com.tu.bzahov.exceptions.EmptySetException;
+import com.tu.bzahov.exceptions.MyEventException;
 import com.tu.bzahov.interfaces.ICompetition;
 
 public class App {
@@ -8,6 +11,31 @@ public class App {
 
     public static void main(String[] args) {
         // TODO: Add main function
+        Participant part1 = new Participant(0,22,"Pesho");
+        Participant part2 = new Participant(1,22,"Gosho");
+        Participant part3 = new Participant(2,22,"Ivan");
+
+        Participant[] partArr = new Participant[3];
+        partArr[0]= part1;
+        partArr[1]= part2;
+        partArr[2]= part3;
+        CreateEvent createEvent = new CreateEvent();
+
+        try {
+            FootballCompetition footballCompetition =(FootballCompetition) createEvent.getEvent("FootballCompetition");
+            CreateEvent.fillSchedule(footballCompetition, partArr);
+            System.out.println("Test for write data: array length: " + footballCompetition.getSchedule().length);
+            System.out.println();
+
+            bets(footballCompetition,1,2);
+            bets(footballCompetition,1,8);
+            bets(footballCompetition,1,118);
+
+        } catch (MyEventException e) {
+            System.out.println(e.getMessage());
+        } catch (EmptySetException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void bets(ICompetition competition, int id, double bet) throws NullPointerException{
@@ -15,31 +43,40 @@ public class App {
         Participant[] schedule = competition.getSchedule();
         Participant currentParticipant = schedule[id];
 
-        double curBetForWin = currentParticipant.getBetForWin();
-        double curChanceForWin = currentParticipant.getChanceForWin();
+        double oldBet = currentParticipant.getBetForWin();
+        double curChance = currentParticipant.getChanceForWin();
 
-        double newBetForWin = calculateNewBetForWin(curBetForWin,bet);
-        currentParticipant.setBetForWin(newBetForWin);
+        double newBet = calculateNewBetForWin(oldBet,bet);
+        currentParticipant.setBetForWin(newBet);
+        System.out.println("Bet for Win: old|new |" + oldBet+ "|"+ newBet+"|");
 
-        double newChanceForWin = calculateNewChanceForWin(curBetForWin,newBetForWin, MINIMUM_CHANCE_FOR_WIN);
-        currentParticipant.setChanceForWin(newChanceForWin);
+        double newChance = calculateNewChanceForWin(curChance,oldBet,newBet);
+        currentParticipant.setChanceForWin(newChance);
+        System.out.println("Chance  Win: old|new |" + curChance+ "|"+ newChance + "|");
+        System.out.println();
     }
 
-    private static double calculateNewBetForWin(double curBetForWin, double bet) {
-        double newBetForWin = 0.0;
-        // TODO: 20.11.18 Complete calculation
-        return newBetForWin;
-    }
-
-    private static double calculateNewChanceForWin(double curBetForWin, double newBetForWin, double minimumChanceForWin) {
-        double newChanceForWin = 0.0;
-        //value less than 0 if d1 is numerically less than d2
-
-        // TODO: 20.11.18 Complete calculation
-        if (Double.compare(newBetForWin,minimumChanceForWin)<0){
-            newChanceForWin = 1.1;
+    private static double calculateNewBetForWin(double oldBet, double bet) {
+        if (oldBet == 0){
+            return bet;
         }
-        return newChanceForWin;
+        return (oldBet + bet) / oldBet;
+    }
+
+    private static double calculateNewChanceForWin(double oldChance, double oldBet, double newBet) {
+        double percentageComponent;
+        if (oldBet == 0){
+            percentageComponent = 0.0;
+        }else percentageComponent = ((newBet - oldBet) / oldBet);
+
+        double newChance = oldChance - percentageComponent;
+        //value less than 0 if d1 is numerically less than d2
+        int compare = Double.compare(newChance, App.MINIMUM_CHANCE_FOR_WIN);
+        System.err.println(newBet+"|"+ App.MINIMUM_CHANCE_FOR_WIN +"| "+ compare);
+        if (compare < 0){
+            newChance = 1.1;
+        }
+        return newChance;
     }
 
 }
